c70d765021715f67e2618de72dee4329815b0e37
MAKEROV Mikhail A <Mikhail.MAKEROV@raiffeisen.ru>
2025-02-11 21:28:18 +0600

    task 6 resolved

--- module_1/lesson_9/app.py
+++ module_1/lesson_9/app.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from datetime import date, timedelta
+# from pydantic import BaseModel
 
 app = FastAPI()
 
+# class Validate(BaseModel):
+#     current_date: date
+#     offset: int
+
+# task 4
 @app.get('/')
 def say_hello():
     return 'hello, world'
 
+# task 5
 @app.get('/')
 def sum_two(a: int, b: int):
     return a + b
+
+# task 6
+@app.get('/sum_date')
+def sum_date(current_date: date, offset: int):
+    # валидация входных данных
+    if isinstance(current_date, date) and isinstance(offset, int):
+        return current_date + timedelta(days=offset)
 
